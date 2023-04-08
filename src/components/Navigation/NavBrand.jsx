d4e4ba680e41fef32e2d4869c13ed0bbd304e96b
VincenzoNetesi <v.marcovecchio@netesi365.com>
2023-04-08 19:34:48 +0200

    change

--- src/components/Navigation/NavBrand.jsx
+++ src/components/Navigation/NavBrand.jsx
@@ -4,13 +4,8 @@ import AutoLink from "../AutoLink/AutoLink";
 const NavBrand = ({ brand, title }) => (
 	<div className="brand">
 		<AutoLink className="flex align-items-center" to="/">
-			<img
-				className="image margin-right-half border-radius"
-				src={brand}
-				alt="Brand Image"
-			/>
 			<span className="title">
-				<strong>Science Bloom</strong>
+				<strong>It's made of science</strong>
 			</span>
 		</AutoLink>
 	</div>
