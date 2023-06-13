65a755ab943cdd37141acdac909a0b9cdbec874a
VincenzoMarcovecchio <vinceoldmark@gmail.com>
2023-06-13 14:21:58 +0200

    change

--- src/templates/food.jsx
+++ src/templates/food.jsx
@@ -20,68 +20,6 @@ function Food({ pageContext }) {
 	return (
 		<>
 			<Layout>
-				<GatsbySeo
-					title={`${pageContext.title} | It's made of science`}
-					description={pageContext.content}
-					canonical={`https://www.madeofscience.com/${pageContext.title.replace(
-						/\s+/g,
-						"-"
-					)}`}
-					openGraph={{
-						url: `https://www.madeofscience.com/${pageContext.title.replace(
-							/\s+/g,
-							"-"
-						)}`,
-						title: `${pageContext.title}`,
-						description: `${pageContext.content}`,
-						site_name: "Made Of Science",
-						type: "article",
-						article: {
-							publishedTime: "2023-06-21T23:04:13Z",
-							modifiedTime: "2023-01-21T18:04:43Z",
-							expirationTime: "2025-12-21T22:04:11Z",
-							section: "Recipes",
-							authors: ["https://www.vincenzo.codes/about"],
-							tags: [
-								"Ricette",
-								"Ricette Abruzzesi",
-								"Cucina",
-								"cooking",
-								"homemade food",
-							],
-						},
-						images: [
-							{
-								url: `${pageContext.image}`,
-								width: 850,
-								height: 650,
-								alt: `${pageContext.title}`,
-							},
-						],
-					}}
-					twitter={{
-						handle: "Vincenzo Marcovecchio",
-						site: "www.madeofscience.com",
-						cardType: "summary_large_image",
-					}}
-				/>
-
-				<BlogPostJsonLd
-					url={`https://www.madeofscience.com/${pageContext.title.replace(
-						/\s+/g,
-						"-"
-					)}`}
-					title={`${pageContext.title} | Made Of Science`}
-					authorName="madeofscience"
-					description={pageContext.content}
-					images={[
-						`${pageContext.image}`,
-						`${pageContext.image}`,
-						`${pageContext.image}`,
-					]}
-					datePublished="2023-02-05T08:00:00+08:00"
-					dateModified="2023-02-05T09:00:00+08:00"
-				/>
 				<section className="main-container container">
 					<div className="content-wrapper padding-top-half padding-bottom-2">
 						<header className="border-bottom border-color-light-grey">
